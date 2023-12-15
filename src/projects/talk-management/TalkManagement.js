051ef157dc2f9b941d66c4134b5c7fc21b9c051d
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-12-15 23:20:19 +0700

    done talk management

--- src/projects/talk-management/TalkManagement.js
+++ src/projects/talk-management/TalkManagement.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const TalkManagement = () => {
   return (
     <div className="wrapper">
-      <header className="pt-10">
+      <header className="pt-10 pb-52">
         <div className="container">
           <nav className="flex items-center justify-between">
             <div>
@@ -32,26 +32,39 @@ const TalkManagement = () => {
               <button className="sign-in">Sign In</button>
             </div>
           </nav>
-          <h1 className="header__heading ">
-            Make{" "}
-            <span className="relative header__heading--highlight">
-              Management
-              <div className="absolute top-0 right-0 w-[10px] h-[9px] rounded-t-full rounded-br-full bg-[#EF8F00] translate-x-full -translate-y-full"></div>
-              <div className="absolute bottom-0 right-0 w-[10px] h-[9px] rounded-b-full rounded-tr-full bg-[#0CCE84] translate-x-full translate-y-full"></div>
-              <div className="absolute top-0 left-0 w-[10px] h-[9px] rounded-t-full rounded-bl-full bg-[#AE8FF9] -translate-x-full -translate-y-full"></div>
-              <div className="absolute bottom-0 left-0 w-[10px] h-[9px] rounded-b-full rounded-tl-full bg-[#FF7667] -translate-x-full translate-y-full"></div>
-            </span>{" "}
-            Way More Easier Than Ever
-          </h1>
-          <p className="header__desc">
-            The online collaborative management platform to bring more
-            efficiency and performace
-          </p>
-          <div className="header__action">
-            <button className="header__btn header__highlight">
-              Request Demo
-            </button>
-            <button className="header__btn">Request Demo</button>
+          <div className="px-7">
+            <h1 className="header__heading ">
+              Make{" "}
+              <span className="relative header__heading--highlight">
+                Management
+                <div className="absolute top-0 right-0 w-[10px] h-[9px] rounded-t-full rounded-br-full bg-[#EF8F00] translate-x-full -translate-y-full"></div>
+                <div className="absolute bottom-0 right-0 w-[10px] h-[9px] rounded-b-full rounded-tr-full bg-[#0CCE84] translate-x-full translate-y-full"></div>
+                <div className="absolute top-0 left-0 w-[10px] h-[9px] rounded-t-full rounded-bl-full bg-[#AE8FF9] -translate-x-full -translate-y-full"></div>
+                <div className="absolute bottom-0 left-0 w-[10px] h-[9px] rounded-b-full rounded-tl-full bg-[#FF7667] -translate-x-full translate-y-full"></div>
+              </span>{" "}
+              Way More Easier Than Ever
+            </h1>
+            <p className="header__desc">
+              The online collaborative management platform to bring more
+              efficiency and performace
+            </p>
+            <div className="header__action">
+              <button className="header__btn header__highlight">
+                Request Demo
+              </button>
+              <button className="header__btn">Contact Us</button>
+            </div>
+            <div className="relative flex justify-center mt-28">
+              <img
+                srcSet="assets/images/projects/talk-management/Task_management_card.png 2x"
+                alt=""
+              />
+              <img
+                srcSet="assets/images/projects/talk-management/Card.png 2x"
+                alt=""
+                className="absolute bottom-0 left-0 translate-y-1/2"
+              />
+            </div>
           </div>
         </div>
       </header>
